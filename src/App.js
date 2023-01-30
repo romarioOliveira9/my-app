1bfeb3a060b376871979ddd445ad6ca699f34d21
Romário Campos <romariocampos.develop@gmail.com>
2023-01-30 16:21:32 -0300

    Primeiro Comentário

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <strong> BRANCH DEV</strong>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Primeiro Comentário 
         </p>
         <a
           className="App-link"
