bc038a5d62ed5c544628c7983baa1de634f1319c
Romário Campos <romariocampos.develop@gmail.com>
2023-01-30 16:25:39 -0300

    Segundo comentário

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ function App() {
         <p>
           Primeiro Comentário 
         </p>
+        <p>
+          Segundo Comentário 
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
